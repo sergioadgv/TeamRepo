a6f66e5bb377fa67e55aee5650321afa81cf69c8
ClevelandBraun <lukasjulian.braunschweig@googlemail.com>
2016-11-30 22:45:22 +0100

    First Slide in

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,23 @@
-console.log("Did you get the memo");
+$(document).ready(function(){
+
+	console.log("Did you get the memo");
+
+	$("#menu-trigger").click(function(){
+
+		$(".slide-in-menu").animate({width: 'toggle'});
+
+		$(this).toggleClass("white");
+
+	});
+
+	$("#dropper").click(function(){
+		$(this).toggleClass("open");
+	});
+
+	//$(".menu-hover").hover(function(){
+
+	//})
+
+	console.log("Js complete!");
+
+})
