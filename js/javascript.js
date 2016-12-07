8c40a908f0569afb512dd4949df60f15eecb848e
Vigelandzoon1995 <houseofcarznl@gmail.com>
2016-12-07 01:13:19 +0100

    hey

--- js/javascript.js
+++ js/javascript.js
@@ -28,28 +28,29 @@ $( document ).ready(function() {
 					}}).done(function (response_body) {
 						response_body.results.forEach(function(item){
 							console.log(item )
-							var url = item.poster_path;
-							var image = $(document.createElement('img'));
-							image.attr('src', 'https://image.tmdb.org/t/p/w154'+url);
-	    					// image.attr('class', thumbnail);
-	    					image.addClass('.view');
-	    					image.appendTo('#scifiphoto-container'); 
+							// var url = item.poster_path;
+							// var image = $(document.createElement('img'));
+							// image.attr('src', 'https://image.tmdb.org/t/p/w154'+url);
+	    		// 			// image.attr('class', thumbnail);
+	    		// 			image.addClass('.view');
+	    		// 			image.appendTo('#scifiphoto-container'); 
 
-	    					// var overview = item.overview;
-	    					// var popDiv = $(document.createElement('div'));
-	    					// popDiv.addClass('view');
-	    					// popDiv.appendTo('#scifiphoto-container');
-	    					// overview.appendTo('.view');
+		    					var overview = item.overview;
+		    					var popDiv = $(document.createElement('div'));
+		    					popDiv.addClass('view');
+		    					popDiv.appendTo('#scifiphoto-container');
+	    					   	var para = $(document.createElement('p'));
+	    					   	para.appendTo('.view');
+	    			
 
 
-	    					$(image).hover(function(){
-	    						// $(this).popover('view');
+	    					// $(image).mouseover(function(){
+	    					// 	$(image).click(function(){
+	    					// 		$('view').fadeIn('slow');
+	    					// 	});
 
-	    					});
+	    					// });
 	    					// console.log(overview);
-
-	    					// overview.addClass('.view');
-	    					// overview.appendTo('body');
 						});
 				
             	
